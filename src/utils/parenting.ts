5187364aa022ee140b5876630eea54fefdb71320
jniac <joseph.merdrignac@gmail.com>
2024-09-27 10:27:33 +0200

    (fix, details) - dispose orbit controls - angle declaration: enabling fraction eg. "1/12turn" - utils/parenting: isChildOf, isParentOf

--- src/utils/parenting.ts
+++ src/utils/parenting.ts
@@ -7,3 +7,18 @@ export function addTo<T extends Object3D>(child: T, parent: Object3D, transformP
   applyTransform(child, transformProps)
   return child
 }
+
+export function isChildOf(child: Object3D, parent: Object3D): boolean {
+  let current = child
+  while (current.parent) {
+    if (current.parent === parent) {
+      return true
+    }
+    current = current.parent
+  }
+  return false
+}
+
+export function isParentOf(parent: Object3D, child: Object3D): boolean {
+  return isChildOf(child, parent)
+}
