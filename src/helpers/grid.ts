0f84b3ca3e122b0f4a09171e779bbf5069c67a19
jniac <joseph.merdrignac@gmail.com>
2024-10-01 05:31:35 +0200

    (fix, impr) SimpleGridHelper align + add position

--- src/helpers/grid.ts
+++ src/helpers/grid.ts
@@ -1,6 +1,6 @@
 import { BufferGeometry, ColorRepresentation, LineBasicMaterial, LineSegments, Vector3 } from 'three'
 
-import { fromVector2Declaration, Vector2Declaration } from '../declaration'
+import { fromVector2Declaration, fromVector3Declaration, Vector2Declaration, Vector3Declaration } from '../declaration'
 
 const defaultSimpleGridProps = {
   color: <ColorRepresentation>'white',
@@ -8,6 +8,7 @@ const defaultSimpleGridProps = {
   divisions: <Vector2Declaration | undefined>undefined,
   align: <Vector2Declaration>[.5, .5],
   opacity: .5,
+  position: <Vector3Declaration | undefined>undefined,
 }
 
 export class SimpleGridHelper extends LineSegments<BufferGeometry, LineBasicMaterial> {
@@ -23,6 +24,7 @@ export class SimpleGridHelper extends LineSegments<BufferGeometry, LineBasicMate
       size: sizeArg,
       divisions: divisionsArg,
       align: alignArg,
+      position: positionArg,
     } = { ...defaultSimpleGridProps, ...props }
 
     const size = fromVector2Declaration(sizeArg)
@@ -32,8 +34,8 @@ export class SimpleGridHelper extends LineSegments<BufferGeometry, LineBasicMate
     const points = [] as Vector3[]
     const push = (x: number, y: number) => points.push(new Vector3(x, y, 0))
 
-    const alignX = (align.x + .5) * size.x
-    const alignY = (align.y + .5) * size.y
+    const alignX = (.5 - align.x) * size.x
+    const alignY = (.5 - align.y) * size.y
 
     for (let i = 0; i <= divisions.x; i++) {
       const x = (i / divisions.x - align.x) * size.x
@@ -52,5 +54,9 @@ export class SimpleGridHelper extends LineSegments<BufferGeometry, LineBasicMate
     this.material.color.set(color)
     this.material.opacity = opacity
     this.material.transparent = opacity < 1
+
+    if (positionArg) {
+      fromVector3Declaration(positionArg, this.position)
+    }
   }
 }
