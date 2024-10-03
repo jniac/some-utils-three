905f14fb9c2f0b0c7e40e900f900b0c1b747649c
jniac <joseph.merdrignac@gmail.com>
2024-10-03 10:26:15 +0200

    (rfctr) "addTo" -> "setup" “Setup” is a fairly generic name, admittedly, but that's really what it does, so it seems preferable to ‘addTo’.

--- src/utils/tree.ts
+++ src/utils/tree.ts
@@ -2,19 +2,43 @@ import { Object3D } from 'three'
 
 import { applyTransform, TransformProps } from './tranform'
 
-export function addTo<T extends Object3D>(
+type SetupParentOrTransformProps = Object3D | TransformProps | null
+type SetupCallback<T extends Object3D> = (instance: T) => void
+
+/**
+ * Convenient method to setup an Object3D instance. Apply transform props and 
+ * add to parent if needed. For further customization, a callback can be provided.
+ * 
+ * Usage:
+ * ```ts
+ * class MyObject extends Group {
+ *   myMesh1 = setup(new Mesh(geometry, material), this)
+ *   myMesh2 = setup(new Mesh(geometry, material), { parent: this, position: [1, 0, 0] })
+ * }
+ * ```
+ */
+export function setup<T extends Object3D>(
   child: T,
-  parent: Object3D,
-  transformProps?: TransformProps | null,
-  callback?: (instance: T) => void,
+  transformProps?: SetupParentOrTransformProps,
+  callback?: SetupCallback<T>,
 ): T {
-  parent.add(child)
   if (transformProps) {
-    applyTransform(child, transformProps)
+    if (transformProps instanceof Object3D) {
+      transformProps.add(child)
+    } else {
+      applyTransform(child, transformProps)
+    }
   }
   callback?.(child)
   return child
 }
+
+/**
+ * Backward compatibility
+ * 
+ * @deprecated Use `setup` instead
+ */
+export const addTo = setup
 
 export function isDescendantOf(child: Object3D, parent: Object3D): boolean {
   let current = child
