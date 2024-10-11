a04af6e12a010f7f477eafcba4cf91a611cd1683
jniac <joseph.merdrignac@gmail.com>
2024-10-11 19:50:32 +0200

    vertigo controls: actions (object)

--- src/camera/vertigo/controls.ts
+++ src/camera/vertigo/controls.ts
@@ -1,9 +1,10 @@
-import { Camera, Quaternion, Vector3 } from 'three'
-
-import { handlePointer, PointerButton } from 'some-utils-dom/handle/pointer'
+import { Camera, Euler, Quaternion, Vector3 } from 'three'
 
 import { handleHtmlElementEvent } from 'some-utils-dom/handle/element-event'
+import { handlePointer, PointerButton } from 'some-utils-dom/handle/pointer'
+import { Animation } from 'some-utils-ts/animation'
 import { DestroyableInstance } from 'some-utils-ts/misc/destroy'
+
 import { Vertigo, VertigoProps } from '../vertigo'
 
 const _quaternion = new Quaternion()
@@ -12,6 +13,51 @@ const _vector1 = new Vector3()
 
 export class VertigoControls extends DestroyableInstance {
   vertigo = new Vertigo()
+
+  actions = {
+    togglePerspective: () => {
+      const perspective = this.vertigo.perspective > .5 ? 0 : 1
+      Animation
+        .tween({
+          target: [this.vertigo, 'perspective'],
+          to: { perspective },
+          duration: 1,
+          ease: 'inOut3',
+        })
+    },
+    rotate: (pitch: number, yaw: number, roll: number) => {
+      const qStart = new Quaternion().setFromEuler(this.vertigo.rotation)
+      const qEnd = new Quaternion().setFromEuler(new Euler(pitch, yaw, roll, 'YXZ'))
+      const q = new Quaternion()
+      Animation
+        .during({
+          target: [this.vertigo, 'rotation'],
+          duration: 1,
+        })
+        .onUpdate(({ progress }) => {
+          q.slerpQuaternions(qStart, qEnd, Animation.ease('inOut3')(progress))
+          this.vertigo.rotation.setFromQuaternion(q)
+        })
+    },
+    positiveXAlign: () => {
+      this.actions.rotate(0, Math.PI / 2, 0)
+    },
+    negativeXAlign: () => {
+      this.actions.rotate(0, -Math.PI / 2, 0)
+    },
+    positiveYAlign: () => {
+      this.actions.rotate(-Math.PI / 2, 0, 0)
+    },
+    negativeYAlign: () => {
+      this.actions.rotate(Math.PI / 2, 0, 0)
+    },
+    positiveZAlign: () => {
+      this.actions.rotate(0, 0, 0)
+    },
+    negativeZAlign: () => {
+      this.actions.rotate(0, Math.PI, 0)
+    },
+  }
 
   constructor(props: VertigoProps = {}) {
     super()
