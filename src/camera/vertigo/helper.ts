c33c232c73748a4ee16019a31a2e2b812e014b36
jniac <joseph.merdrignac@gmail.com>
2024-10-14 00:57:00 +0200

    VertigoHelper: v0

---
+++ src/camera/vertigo/helper.ts
@@ -0,0 +1,44 @@
+import { ColorRepresentation, DoubleSide, LineBasicMaterial, Mesh, MeshBasicMaterial, PlaneGeometry, Texture } from 'three'
+import { LineHelper } from '../../helpers/line'
+import { Vertigo } from '../vertigo'
+
+const canvas = document.createElement('canvas')
+const ctx = canvas.getContext('2d')!
+canvas.width = 1024
+canvas.height = 256
+ctx.fillStyle = 'white'
+ctx.font = '200px Fira Code'
+ctx.textBaseline = 'top'
+ctx.fillText('Vertigo', 20, 20)
+
+const texture = new Texture(canvas)
+texture.needsUpdate = true
+
+export class VertigoHelper extends LineHelper {
+  constructor(vertigo: Vertigo, { color = <ColorRepresentation>'red' } = {}) {
+    super(undefined, new LineBasicMaterial({ color }))
+
+    this.position.copy(vertigo.focus)
+    this.rotation.copy(vertigo.rotation)
+
+    const { x: sx, y: sy } = vertigo.size
+    this
+      .rectangle([-sx / 2, -sy / 2, sx, sy])
+      .plus([0, 0], .5)
+      .draw()
+
+    const plane = new Mesh(
+      new PlaneGeometry(1, .25),
+      new MeshBasicMaterial({
+        color,
+        alphaMap: texture,
+        transparent: true,
+        side: DoubleSide,
+      }),
+    )
+    const padding = .1
+    plane.position.set(-sx / 2 + .5 + padding, sy / 2 - .125 - padding, 0)
+
+    this.add(plane)
+  }
+}
