a9d2e5d63bcdc2f45273a8bb00a6c4b3228b2f5c
jniac <joseph.merdrignac@gmail.com>
2024-12-12 22:25:56 +0100

    /webgpu

---
+++ src/webgpu/utils/transform.ts
@@ -0,0 +1,114 @@
+import { Euler, Object3D, Vector3 } from 'three/webgpu'
+
+import { AngleDeclaration, AngleUnit, EulerDeclaration, fromEulerDeclaration, fromVector3Declaration, Vector3Declaration } from '../declaration'
+
+const defaultTransform = {
+  position: new Vector3(0, 0, 0),
+  rotation: new Euler(0, 0, 0, 'XYZ'),
+  scale: new Vector3(1, 1, 1),
+  visible: true,
+}
+
+export type Transform = typeof defaultTransform
+
+const defaultTransformProps = {
+  x: 0,
+  y: 0,
+  z: 0,
+
+  rotationX: <AngleDeclaration>0,
+  rotationY: <AngleDeclaration>0,
+  rotationZ: <AngleDeclaration>0,
+  rotationOrder: <Euler['order']>'XYZ',
+  rotationUnit: <AngleUnit>'rad',
+
+  scaleX: 1,
+  scaleY: 1,
+  scaleZ: 1,
+  scaleScalar: 1,
+
+  // Extra:
+  /**
+   * Applies only if defined.
+   * 
+   * Defaults to `undefined`.
+   */
+  visible: <boolean | undefined>undefined,
+  /**
+   * Applies only if defined.
+   * 
+   * Defaults to `undefined`.
+   */
+  name: <string | undefined>undefined,
+  /**
+   * Applies only if defined.
+   * 
+   * Defaults to `undefined`.
+   */
+  parent: <Object3D | undefined>undefined,
+}
+
+export type TransformProps = Partial<typeof defaultTransformProps & {
+  position: Vector3 | Vector3Declaration
+  rotation: Euler | EulerDeclaration
+  scale: Vector3 | Vector3Declaration
+}>
+
+export function applyTransform<T extends Object3D = Object3D>(target: T, props?: TransformProps): T {
+  const {
+    x,
+    y,
+    z,
+    position = new Vector3(x, y, z),
+
+    rotationX,
+    rotationY,
+    rotationZ,
+    rotationOrder,
+    rotationUnit,
+    rotation,
+
+    scaleX,
+    scaleY,
+    scaleZ,
+    scaleScalar,
+    scale = new Vector3(scaleX, scaleY, scaleZ).multiplyScalar(scaleScalar),
+
+    visible,
+    name,
+    parent,
+  } = { ...defaultTransformProps, ...props }
+
+  fromVector3Declaration(position, target.position)
+  fromEulerDeclaration(rotation ?? [rotationX, rotationY, rotationZ, rotationOrder, rotationUnit], target.rotation)
+  fromVector3Declaration(scale, target.scale)
+
+  if (visible !== undefined) {
+    target.visible = visible
+  }
+
+  if (name !== undefined) {
+    target.name = name
+  }
+
+  if (parent !== undefined) {
+    if (parent !== target.parent) {
+      if (parent === null) {
+        target.removeFromParent()
+      } else {
+        parent.add(target)
+      }
+    }
+  }
+
+  return target
+}
+
+export function getTransform(target: Object3D): Transform {
+  return {
+    position: target.position.clone(),
+    rotation: target.rotation.clone(),
+    scale: target.scale.clone(),
+    visible: target.visible,
+  }
+}
---
+++ src/webgpu/utils/tree.ts
@@ -0,0 +1,57 @@
+import { Object3D } from 'three/webgpu'
+
+import { isObject3D } from '../../is'
+import { applyTransform, TransformProps } from './transform'
+
+type SetupParentOrTransformProps = Object3D | TransformProps | null
+type SetupCallback<T extends Object3D> = (instance: T) => void
+
+/**
+ * Convenient method to setup an Object3D instance. Apply transform props and 
+ * add to parent if needed. For further customization, a callback can be provided.
+ * 
+ * Usage:
+ * ```ts
+ * class MyObject extends Group {
+ *   myMesh1 = setup(new Mesh(geometry, material), this)
+ *   myMesh2 = setup(new Mesh(geometry, material), { parent: this, position: [1, 0, 0] })
+ * }
+ * ```
+ */
+export function setup<T extends Object3D>(
+  child: T,
+  transformProps?: SetupParentOrTransformProps,
+  callback?: SetupCallback<T>,
+): T {
+  if (transformProps) {
+    if (isObject3D(transformProps)) {
+      transformProps.add(child)
+    } else {
+      applyTransform(child, transformProps)
+    }
+  }
+  callback?.(child)
+  return child
+}
+
+/**
+ * Backward compatibility
+ * 
+ * @deprecated Use `setup` instead
+ */
+export const addTo = setup
+
+export function isDescendantOf(child: Object3D, parent: Object3D): boolean {
+  let current = child
+  while (current.parent) {
+    if (current.parent === parent) {
+      return true
+    }
+    current = current.parent
+  }
+  return false
+}
+
+export function isAncestorOf(parent: Object3D, child: Object3D): boolean {
+  return isDescendantOf(child, parent)
+}
