3bd41689e6cbea5ad7e0dfc0f56db99196f3e192
jniac <joseph.merdrignac@gmail.com>
2025-05-15 18:45:53 +0200

    better debug-helper: "key" option allows to redraw "in place"

---
+++ src/helpers/debug/base.ts
@@ -0,0 +1,25 @@
+import { Matrix4 } from 'three'
+
+import { TransformDeclaration } from '../../declaration'
+
+export class BaseManager {
+  keyMap = new Map<any, { index: number; count: number }>()
+
+  ensureKeyEntry(key: any, index: number, count: number) {
+    const entry = this.keyMap.get(key)
+    if (entry) {
+      if (entry.count !== count)
+        throw new Error('A key entry already exists with a different count. This is not allowed. Once a key is set, it cannot be changed.')
+      return entry
+    } else {
+      const newEntry = { index, count }
+      this.keyMap.set(key, newEntry)
+      return newEntry
+    }
+  }
+
+  transformMatrix = new Matrix4()
+  applyTransform(...transforms: TransformDeclaration[]) {
+    throw new Error('Not implemented!')
+  }
+}
