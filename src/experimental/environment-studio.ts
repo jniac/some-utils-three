d50aa40581413b767ceaade5f8056f1e66f6906f
jniac <joseph.merdrignac@gmail.com>
2025-07-29 19:12:03 +0200

    environment render supports mixing scenes!!!

--- src/experimental/environment-studio.ts
+++ src/experimental/environment-studio.ts
@@ -1,4 +1,4 @@
-import { BackSide, BoxGeometry, Color, ColorRepresentation, CubeCamera, FloatType, Group, IcosahedronGeometry, Mesh, MeshBasicMaterial, MeshPhysicalMaterial, PlaneGeometry, PointLight, RGBAFormat, Scene, ShaderMaterial, SphereGeometry, TorusGeometry, UnsignedByteType, Vector3Tuple, WebGLCubeRenderTarget, WebGLRenderer } from 'three'
+import { BackSide, BoxGeometry, Color, ColorRepresentation, CubeCamera, FloatType, Group, IcosahedronGeometry, Mesh, MeshBasicMaterial, MeshPhysicalMaterial, PlaneGeometry, PointLight, RGBAFormat, Scene, ShaderMaterial, SphereGeometry, Texture, TorusGeometry, UnsignedByteType, Vector3Tuple, WebGLCubeRenderTarget, WebGLRenderer } from 'three'
 import { RoundedBoxGeometry } from 'three/examples/jsm/Addons.js'
 
 import { lazy } from 'some-utils-ts/lazy'
@@ -17,15 +17,6 @@ function hashString(str: string): number {
 
 function array<T>(length: number, value: (i: number) => T) {
   return Array.from({ length }, (_, i) => value(i))
-}
-
-function isWebGL2Supported() {
-  try {
-    const canvas = document.createElement('canvas')
-    return !!(window.WebGL2RenderingContext && canvas.getContext('webgl2'))
-  } catch (e) {
-    return false
-  }
 }
 
 /**
@@ -109,20 +100,72 @@ export class EnvironmentStudio {
 
   static createParts(textureSize: number) {
     const scene = new Scene()
+    const altScene = new Scene()
 
     const cap = capabilities()
     const type = cap.isFloatTextureSupported ? FloatType : UnsignedByteType
+    const format = RGBAFormat
 
     const rt = new WebGLCubeRenderTarget(textureSize, {
       generateMipmaps: true,
       type,
-      format: RGBAFormat,
+      format,
     })
+
+    const rt0 = new WebGLCubeRenderTarget(textureSize, {
+      generateMipmaps: false,
+      type,
+      format,
+    })
+
+    const rt1 = new WebGLCubeRenderTarget(textureSize, {
+      generateMipmaps: false,
+      type,
+      format,
+    })
+
     const cubeCamera = new CubeCamera(.1, 100, rt)
+
+    const mixScene = new Scene()
+    const mixMaterial = new ShaderMaterial({
+      uniforms: {
+        uAlpha: { value: 0 },
+        uEnv0: { value: rt0.texture },
+        uEnv1: { value: rt1.texture },
+      },
+      vertexShader: /* glsl */`
+        varying vec3 vWorldDirection;
+        void main() {
+          vWorldDirection = normalize(position);
+          gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+        }
+      `,
+      fragmentShader: /* glsl */`
+        varying vec3 vWorldDirection;
+        uniform float uAlpha;
+        uniform samplerCube uEnv0;
+        uniform samplerCube uEnv1;
+        void main() {
+          vec4 color0 = texture(uEnv0, vWorldDirection);
+          vec4 color1 = texture(uEnv1, vWorldDirection);
+          gl_FragColor = mix(color0, color1, uAlpha);
+        }
+      `,
+      side: BackSide,
+    })
+    const mixCube = new Mesh(new BoxGeometry(2, 2, 2), mixMaterial)
+    mixScene.add(mixCube)
+
     return {
-      rt,
       cubeCamera,
       scene,
+      altScene,
+      mixScene,
+      mixMaterial,
+      mixCube,
+      rt,
+      rt0,
+      rt1,
     }
   }
 
@@ -141,6 +184,29 @@ export class EnvironmentStudio {
     this.parts = EnvironmentStudio.createParts(textureSize)
   }
 
+  /**
+   * Swap the main scene with the alternate scene.
+   * 
+   * This is useful for rendering two different environments and mixing them:
+   * ```
+   * studio.createEnvironmentSphere({ map: texture1 })
+   * studio.swapScenes()
+   * studio.createEnvironmentCube({ map: texture2 })
+   * studio.renderMix(renderer, 0.5)
+   * ```
+   */
+  swapScenes() {
+    const { scene, altScene } = this.parts
+    this.parts.scene = altScene
+    this.parts.altScene = scene
+    return this
+  }
+
+  /**
+   * Render the main scene.
+   * 
+   * For mixing environments, use `renderMix` instead.
+   */
   render(renderer: WebGLRenderer, deltaTime = 1 / 60) {
     this.#state.deltaTime = deltaTime
 
@@ -155,19 +221,74 @@ export class EnvironmentStudio {
     return rt.texture
   }
 
+  /**
+   * Render the 2 scenes (`scene` and `altScene`) and mix them based on the `alpha` value.
+   * 
+   * Note:
+   * - If `alpha` is close to 0, it will render the `scene` directly.
+   * - If `alpha` is close to 1, it will render the `altScene` directly.
+   * - Otherwise, it will render both scenes and mix them based on the `alpha` value.
+   */
+  renderMix(renderer: WebGLRenderer, alpha: number, deltaTime = 1 / 60) {
+    this.parts.rt.texture.generateMipmaps = false
+
+    // If alpha is close to 0, skip the mix and render the scene directly
+    if (alpha < 1e-5) {
+      this.render(renderer, deltaTime)
+      return this.parts.rt.texture
+    }
+
+    // If alpha is close to 1, render the mix scene and swap back
+    if (alpha > 1 - 1e-5) {
+      this.swapScenes()
+      this.render(renderer, deltaTime)
+      this.swapScenes()
+      return this.parts.rt.texture
+    }
+
+    for (const callback of this.#state.onRender)
+      callback()
+
+    const { cubeCamera, scene, altScene, mixScene, mixMaterial, rt, rt0, rt1 } = this.parts
+    cubeCamera.renderTarget = rt0
+    cubeCamera.update(renderer, scene)
+    cubeCamera.renderTarget = rt1
+    cubeCamera.update(renderer, altScene)
+
+    cubeCamera.renderTarget = rt
+    mixMaterial.uniforms.uEnv0.value = rt0.texture
+    mixMaterial.uniforms.uEnv1.value = rt1.texture
+    mixMaterial.uniforms.uAlpha.value = alpha
+    cubeCamera.update(renderer, mixScene)
+
+    this.#state.time += deltaTime
+
+    return rt.texture
+  }
+
   clear() {
     this.#state.onRender.clear()
     this.#state.time = 0
     this.#state.deltaTime = 0
-    this.parts.scene.traverse(object => {
-      if (object instanceof Mesh) {
-        object.geometry.dispose()
-        object.material.dispose()
-      }
-    })
-    this.parts.scene.clear()
+
+    const clear = (scene: Scene) => {
+      scene.traverse(object => {
+        if (object instanceof Mesh) {
+          object.geometry.dispose()
+          object.material.dispose()
+        }
+      })
+      scene.clear()
+      scene.background = null
+    }
+
+    clear(this.parts.scene)
+    clear(this.parts.altScene)
+
     return this
   }
+
+
 
   createSolidCube({
     size = 10,
@@ -294,6 +415,46 @@ export class EnvironmentStudio {
     return {
       sphere: setup(new Mesh(geometry, material), this.parts.scene),
     }
+  }
+
+  /**
+   * Create a sphere that can be used as an environment map.
+   * 
+   * Note:
+   * - The map will be projected "equirectangularly" onto the sphere.
+   */
+  createEnvironmentSphere({
+    size = 10,
+    map = null as null | Texture,
+  }) {
+    const geometry = new SphereGeometry(size * 1.33 / 2, 8, 8)
+    const material = new MeshBasicMaterial({
+      map,
+      side: BackSide,
+      depthWrite: false,
+    })
+    const environmentMesh = setup(new Mesh(geometry, material), this.parts.scene)
+    return { environmentMesh }
+  }
+
+  /**
+   * Create a cube that can be used as an environment map.
+   * 
+   * Note:
+   * - The map will be applied on each face of the cube.
+   */
+  createEnvironmentCube({
+    size = 10,
+    map = null as null | Texture,
+  }) {
+    const geometry = new BoxGeometry(size, size, size)
+    const material = new MeshBasicMaterial({
+      map,
+      side: BackSide,
+      depthWrite: false,
+    })
+    const environmentMesh = setup(new Mesh(geometry, material), this.parts.scene)
+    return { environmentMesh }
   }
 
   createGround({
