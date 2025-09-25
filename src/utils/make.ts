c37b254c6618db076e38ecf01c060f20149c3d3e
jniac <joseph.merdrignac@gmail.com>
2025-09-25 10:49:35 +0200

    makeColor: auto trim alpha channel from hexstring

--- src/utils/make.ts
+++ src/utils/make.ts
@@ -25,11 +25,28 @@ const _color = new Color()
  * Returns a color instance from the given color representation.
  * 
  * NOTE: 
+ * - For convenience, the string 'random' can be passed to get a random color.
+ * - For convenience too, 8-digit hex colors are supported (the alpha channel is ignored).
  * - For performance reasons, by default the same color instance is returned.
  * The value must be used immediately or copied / cloned if it needs to be stored.
  * - To get a new instance, pass a new color as the second argument.
  */
-export function makeColor(color: ColorRepresentation, out = _color): Color {
+export function makeColor(color: ColorRepresentation | 'random', out = _color): Color {
+  if (color === 'random') {
+    out.setHex(Math.floor(Math.random() * 0x1000000))
+    return out
+  }
+  if (typeof color === 'string') {
+    if (color.startsWith('#')) {
+      // three.js does not support 8-digit hex colors, so we remove the alpha channel if present
+      if (color.length === 9) {
+        color = color.slice(0, 7)
+      }
+      if (color.length === 5) {
+        color = color.slice(0, 4)
+      }
+    }
+  }
   return out.set(color)
 }
 
