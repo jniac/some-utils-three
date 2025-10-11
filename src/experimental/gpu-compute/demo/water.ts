37bce2ae813e048679ce56b9f70dfc7676e4305c
jniac <joseph.merdrignac@gmail.com>
2025-10-11 12:21:44 +0200

    GpuComputeWaterDemo: "fading margin", cellScale parameter

--- src/experimental/gpu-compute/demo/water.ts
+++ src/experimental/gpu-compute/demo/water.ts
@@ -4,7 +4,8 @@ import { GpuCompute, GpuComputeParams } from '../gpu-compute'
 const defaultParams = {
   ...GpuCompute.defaultParams,
 
-  viscosity: 0.99,
+  viscosity: 0.98,
+  cellScale: 7.0,
 }
 
 type Params = typeof defaultParams & GpuComputeParams
@@ -25,26 +26,38 @@ export class GpuComputeWaterDemo extends GpuCompute<Params> {
       update: {
         uniforms: {
           uViscosity: { value: this.params.viscosity },
+          uCellScale: { value: this.params.cellScale },
           uPointer: { value: new Vector4(.5, .5, 0.1, 1.0) },
         },
+        fragmentTop: /* glsl */`
+          vec4 fetch(vec2 uv) {
+            if (uv.x < 0.0 || uv.x > 1.0 || uv.y < 0.0 || uv.y > 1.0) {
+              return vec4(0.0);
+            }
+            float margin = 0.2;
+            float edgeDist = min(min(uv.x, 1.0 - uv.x), min(uv.y, 1.0 - uv.y));
+            edgeDist = 1.0 - pow(1.0 - edgeDist, 5.0);
+            float edgeFade = smoothstep(0.0, margin, edgeDist);
+            return texture2D(uTexture, uv) * edgeFade;
+          }
+        `,
         fragmentColor: /* glsl */`
           vec2 uv = vUv;
           float viscosity = uViscosity;
-          float scale = 3.0;
-          vec2 cellSize = scale / uTextureSize;
+          vec2 cellSize = uCellScale / uTextureSize;
 
-          vec4 center = texture2D(uTexture, uv);
-          vec4 north = texture2D(uTexture, uv + vec2(0.0, cellSize.y));
-          vec4 south = texture2D(uTexture, uv - vec2(0.0, cellSize.y));
-          vec4 east = texture2D(uTexture, uv + vec2(cellSize.x, 0.0));
-          vec4 west = texture2D(uTexture, uv - vec2(cellSize.x, 0.0));
+          vec4 center = fetch(uv);
+          vec4 north = fetch(uv + vec2(0.0, cellSize.y));
+          vec4 south = fetch(uv - vec2(0.0, cellSize.y));
+          vec4 east = fetch(uv + vec2(cellSize.x, 0.0));
+          vec4 west = fetch(uv - vec2(cellSize.x, 0.0));
 
           // x: previous height
           // y: penultimate height
           // z: unused
           // w: unused
 
-          float newHeight = ((north.x + south.x + east.x + west.x) * 0.5 - center.y) * viscosity;
+          float newHeight = ((north.x + south.x + east.x + west.x) * 0.501 - center.y) * viscosity;
 
           // Pointer influence
           float radius = uPointer.z;
@@ -52,7 +65,7 @@ export class GpuComputeWaterDemo extends GpuCompute<Params> {
           float dist = distance(uv, uPointer.xy) - radius;
           float cellLength = length(cellSize);
           float influence = smoothstep(cellLength * 0.5, -cellLength * 0.5, dist);
-          newHeight += influence * strength * cos(uTime * 10.0);
+          newHeight += influence * strength;
 
           gl_FragColor = vec4(newHeight, center.x, 0.0, 1.0);
         `,
