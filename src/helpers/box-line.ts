79b4fe3035767adcef43743e048ebccce96bab60
jniac <joseph.merdrignac@gmail.com>
2025-10-21 14:37:36 +0200

    helpers: + BoxLineHelper

---
+++ src/helpers/box-line.ts
@@ -0,0 +1,81 @@
+import { BufferAttribute, BufferGeometry, LineBasicMaterial, LineSegments, Vector3, WebGLProgramParametersWithUniforms } from 'three'
+
+import { ShaderForge } from 'some-utils-three/shader-forge'
+
+export class BoxLineHelper extends LineSegments<BufferGeometry, LineBasicMaterial> {
+  constructor(options?: {
+    divisions?: number,
+    interiorOpacity?: number,
+    onBeforeCompile?: (shader: WebGLProgramParametersWithUniforms) => void,
+  }) {
+    const {
+      divisions = 10,
+      interiorOpacity = 0.2,
+    } = options ?? {}
+    const getLerpPoints = (p0: Vector3, p1: Vector3, count: number) => {
+      return Array.from({ length: count }, (_, i) => [
+        new Vector3().lerpVectors(p0, p1, i / count),
+        new Vector3().lerpVectors(p0, p1, (i + 1) / count),
+      ]).flat()
+    }
+    const boxGeometry = new BufferGeometry().setFromPoints([
+      // x edges
+      ...getLerpPoints(new Vector3(-1, -1, -1), new Vector3(+1, -1, -1), divisions),
+      ...getLerpPoints(new Vector3(-1, +1, -1), new Vector3(+1, +1, -1), divisions),
+      ...getLerpPoints(new Vector3(-1, +1, +1), new Vector3(+1, +1, +1), divisions),
+      ...getLerpPoints(new Vector3(-1, -1, +1), new Vector3(+1, -1, +1), divisions),
+
+      // y edges
+      ...getLerpPoints(new Vector3(-1, -1, -1), new Vector3(-1, +1, -1), divisions),
+      ...getLerpPoints(new Vector3(+1, -1, -1), new Vector3(+1, +1, -1), divisions),
+      ...getLerpPoints(new Vector3(+1, -1, +1), new Vector3(+1, +1, +1), divisions),
+      ...getLerpPoints(new Vector3(-1, -1, +1), new Vector3(-1, +1, +1), divisions),
+
+      // z edges
+      ...getLerpPoints(new Vector3(-1, -1, -1), new Vector3(-1, -1, +1), divisions),
+      ...getLerpPoints(new Vector3(+1, -1, -1), new Vector3(+1, -1, +1), divisions),
+      ...getLerpPoints(new Vector3(+1, +1, -1), new Vector3(+1, +1, +1), divisions),
+      ...getLerpPoints(new Vector3(-1, +1, -1), new Vector3(-1, +1, +1), divisions),
+
+      // x interior edges
+      ...getLerpPoints(new Vector3(-1, -1, 0), new Vector3(+1, -1, 0), divisions),
+      ...getLerpPoints(new Vector3(-1, +1, 0), new Vector3(+1, +1, 0), divisions),
+      ...getLerpPoints(new Vector3(-1, 0, -1), new Vector3(+1, 0, -1), divisions),
+      ...getLerpPoints(new Vector3(-1, 0, +1), new Vector3(+1, 0, +1), divisions),
+
+      // y interior edges
+      ...getLerpPoints(new Vector3(0, -1, -1), new Vector3(0, +1, -1), divisions),
+      ...getLerpPoints(new Vector3(0, -1, +1), new Vector3(0, +1, +1), divisions),
+      ...getLerpPoints(new Vector3(-1, -1, 0), new Vector3(-1, +1, 0), divisions),
+      ...getLerpPoints(new Vector3(+1, -1, 0), new Vector3(+1, +1, 0), divisions),
+
+      // z interior edges
+      ...getLerpPoints(new Vector3(-1, 0, -1), new Vector3(-1, 0, +1), divisions),
+      ...getLerpPoints(new Vector3(+1, 0, -1), new Vector3(+1, 0, +1), divisions),
+      ...getLerpPoints(new Vector3(0, -1, -1), new Vector3(0, -1, +1), divisions),
+      ...getLerpPoints(new Vector3(0, +1, -1), new Vector3(0, +1, +1), divisions),
+    ])
+    const opacityAttribute = new BufferAttribute(new Float32Array(boxGeometry.getAttribute('position').count), 1)
+    opacityAttribute.array.fill(1, 0, opacityAttribute.array.length / 2)
+    opacityAttribute.array.fill(interiorOpacity, opacityAttribute.array.length / 2)
+    boxGeometry.setAttribute('aOpacity', opacityAttribute)
+
+    const boxMaterial = new LineBasicMaterial({ color: '#ff0', transparent: true })
+    boxMaterial.onBeforeCompile = shader => {
+      ShaderForge.with(shader)
+        .varying({ vOpacity: 'float' })
+        .vertex.top(/* glsl */`
+          attribute float aOpacity;
+        `)
+        .vertex.mainAfterAll(/* glsl */`
+          vOpacity = aOpacity;
+        `)
+        .fragment.after('map_fragment', /* glsl */`
+          diffuseColor.a *= vOpacity;
+        `)
+      options?.onBeforeCompile?.(shader)
+    }
+
+    super(boxGeometry, boxMaterial)
+  }
+}
