b0e275afb79b6e3c090a938f2299d4b9cdcc0533
jniac <joseph.merdrignac@gmail.com>
2025-10-30 17:48:59 +0100

    SimpleGridHelper: "depthOffset" option

--- src/helpers/grid.ts
+++ src/helpers/grid.ts
@@ -1,25 +1,47 @@
 import { BufferGeometry, ColorRepresentation, LineBasicMaterial, LineSegments, Vector3 } from 'three'
 
 import { fromVector2Declaration, Vector2Declaration } from '../declaration'
+import { ShaderForge } from '../shader-forge'
 
 const defaultSimpleGridProps = {
   color: <ColorRepresentation>'white',
   opacity: .5,
   plane: 'XY' as 'XY' | 'XZ' | 'YZ',
-
   size: <Vector2Declaration>[8, 8],
   step: <Vector2Declaration | undefined>1,
+
   /**
    * Whether to draw a frame around the grid.
    */
   frame: true,
+
+  /**
+   * Depth offset to "cheat" depth (z-fighting issues, background / foreground behavior).
+   */
+  depthOffset: 0,
 }
 
 type SimpleGridProps = Partial<typeof defaultSimpleGridProps>
 
+function createUniforms() {
+  return {
+    uDepthOffset: { value: 0 },
+  }
+}
+
 export class SimpleGridHelper extends LineSegments<BufferGeometry, LineBasicMaterial> {
+  uniforms: ReturnType<typeof createUniforms>
+
   constructor(props?: SimpleGridProps) {
-    super()
+    const material = new LineBasicMaterial()
+    const uniforms = createUniforms()
+    material.onBeforeCompile = shader => ShaderForge.with(shader)
+      .uniforms(uniforms)
+      .vertex.mainAfterAll(/* glsl */`
+        gl_Position.w += uDepthOffset;
+      `)
+    super(undefined, material)
+    this.uniforms = uniforms
     this.set(props ?? defaultSimpleGridProps)
   }
 
@@ -31,6 +53,7 @@ export class SimpleGridHelper extends LineSegments<BufferGeometry, LineBasicMate
       step: stepArg,
       frame,
       plane,
+      depthOffset,
     } = { ...defaultSimpleGridProps, ...props }
 
     const size = fromVector2Declaration(sizeArg)
@@ -89,6 +112,7 @@ export class SimpleGridHelper extends LineSegments<BufferGeometry, LineBasicMate
       }
     }
 
+    this.uniforms.uDepthOffset.value = depthOffset
     this.material.color.set(color)
     this.material.opacity = opacity
     this.material.transparent = opacity < 1
